9d22eff2d3998ac40904e03ee2b62c59c7aeea29
pirognepirog <pirognepirog@gmail.com>
2026-04-26 08:23:59 +0300

    Commit Branch

---
+++ src/Author.java
@@ -0,0 +1,23 @@
+public class Author {
+    private final String surname;
+    private final String name;
+
+
+    public Author(String surname, String name) {
+        this.surname = surname;
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return surname;
+    }
+
+    public String setName() {
+        return name;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,46 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!11q");
+//В методе main создайте несколько объектов «Книга» (достаточно двух) и несколько объектов «Автор» (достаточно тоже двух)
+// и инициализируйте их. Учтите, что авторы являются обязательными и книги не могут создаваться без авторов.
+//Метод main не должен находиться в классах Book и Author.
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("============= СОЗДАЮ ОБЪЕКТЫ =================");
+    Author ivanov = new Author("Ivanov","Ivan");
+    Author pitrov = new Author("Petrov","Petr");
+
+        Book bookIvana = new Book("Book Ivana","Ivanov Compani ltd", 2025);
+        Book bookIvana2 = new Book("Book2 Ivana","Ivanov Compani ltd", 2025);
+
+        System.out.println("bookIvana = " + bookIvana.getAuthor() + " " + bookIvana.getPublisher()
+                + " " + bookIvana.getPublishingYear());
+        System.out.println("bookIvana2 = " + bookIvana2.getAuthor() + " " + bookIvana2.getPublisher()
+                + " " + bookIvana2.getPublishingYear());
+
+        Book bookPetra = new Book("Book Petra","Ivanov Compani ltd", 2025);
+        Book bookPetra2 = new Book("Book2 Petra","Ivanov Compani ltd", 2025);
+
+        System.out.println("bookPetra = " + bookPetra.getAuthor()  + " " + bookPetra.getPublisher()
+                + " " + bookPetra.getPublishingYear());
+        System.out.println("bookPetra2 = " + bookPetra2.getAuthor()  + " " + bookPetra2.getPublisher()
+                + " " + bookPetra2.getPublishingYear());
+
+        System.out.println("============= МЕНЯЮ ЗНАЧАНЕНИЯ В ОБЪЕКТАХ =================");
+        bookIvana.setPublishingYear(1999);
+        bookPetra.setPublishingYear(1995);
+
+        System.out.println("bookIvana = " + bookIvana.getAuthor() + " " + bookIvana.getPublisher()
+                + " " + bookIvana.getPublishingYear());
+        System.out.println("bookPetra = " + bookPetra.getAuthor()  + " " + bookPetra.getPublisher()
+                + " " + bookPetra.getPublishingYear());
+
+        System.out.println("============= ДОПОЛНИТЕЛЬНЫЕ ИЗМЕНЕНИЯ ЗНАЧАНЕНИЙ В ОБЪЕКТАХ =================");
+        bookIvana.setPublisher("- Old Compani Ivanov ltd -");
+        bookPetra.setPublisher("- Old Compani Petrov ltd -");
+
+        System.out.println("bookIvana = " + bookIvana.getAuthor() + " " + bookIvana.getPublisher()
+                + " " + bookIvana.getPublishingYear());
+        System.out.println("bookPetra = " + bookPetra.getAuthor()  + " " + bookPetra.getPublisher()
+                + " " + bookPetra.getPublishingYear());
     }
 }
